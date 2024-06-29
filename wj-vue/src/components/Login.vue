9e10fe8de40f135551630c055d2938945c2aaf84
ytdcggfhg <3380992810.qq.com>
2024-06-29 17:05:44 +0800

    又进行了一次修改

--- wj-vue/src/components/Login.vue
+++ wj-vue/src/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    用户名2222222:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
+    用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
     <br><br>
     密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
     <br><br>
